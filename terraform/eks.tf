965caa4a7621d4ae1cc35b895ed9713ef635c3ba
TafariBeckford <beckfordtafari55@gmail.com>
2023-02-08 16:11:00 -0500

    terraform

---
+++ terraform/eks.tf
@@ -0,0 +1,48 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "devops-the-hard-way-cluster"
+  cluster_version = "1.24"
+
+  cluster_endpoint_public_access  = true
+
+  enable_irsa = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+ 
+
+  eks_managed_node_groups = {
+    devops-node-group = {
+      min_size     = 3
+      max_size     = 3
+      desired_size = 3
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+   node_security_group_additional_rules = {
+    ingress_allow_access_from_control_plane = {
+      type                          = "ingress"
+      protocol                      = "tcp"
+      from_port                     = 9443
+      to_port                       = 9443
+      source_cluster_security_group = true
+      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
+    }
+  }
+}
